1686d3be03ff1303cdcac48b01cdda788dcb09f4
Mariya Romanyak <mary.romanyak@gmail.com>
2019-04-25 01:43:13 +0300

    update

---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+function initMap() {
+// The location of Midtown, USA
+var midtown = {lat: 40.754932, lng: -73.984016};
+// The map, centered at Midtown
+var map = new google.maps.Map(
+  document.getElementById('map'), {zoom: 12, center: midtown});
+// The marker, positioned at Midtown
+var marker = new google.maps.Marker({position: midtown, map: map});
+}
